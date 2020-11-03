bc30a0b7f9ca101ac0f544e4198514af2eae16be
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-11-03 14:31:57 -0700

    Added validations/has_many; built users controller

--- backend/idl/app/controllers/users_controller.rb
+++ backend/idl/app/controllers/users_controller.rb
@@ -1,2 +1,42 @@
 class UsersController < ApplicationController
+    def index
+        users = User.all()
+        return json: users
+    end
+
+    def create
+        user = User.new(user_params)
+
+        if user.save
+            return json: user
+        else
+            return json: {status: "error", errors: [user.errors.full_messages]}
+        end
+    end
+
+    def update
+        user = User.find_by('id': [:params][:user][:id])
+        if !user.empty?
+            user.update(user_params)
+            return json: user
+        else
+            return json: {message: "error - invalid user data"}
+        end
+    end
+
+    def delete
+        user = User.find_by('id': [:params][:user][:id])
+        if !user.nil?
+            user.destroy
+            return json: {message: 'user successfully deleted'}
+        else
+            return json: {message: 'couldn\'t find user'}
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
--- backend/idl/app/models/video.rb
+++ backend/idl/app/models/video.rb
@@ -1,2 +1,6 @@
 class Video < ApplicationRecord
+    belongs_to :user
+
+    validates [:url, :name], uniqueness: true
+    
 end
