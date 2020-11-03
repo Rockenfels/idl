1822b3d1cc096f26a45cf6f32e00e94439182d51
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-11-03 14:06:41 -0700

    generated user and video resources

---
+++ backend/idl/db/migrate/20201103210404_create_videos.rb
@@ -0,0 +1,11 @@
+class CreateVideos < ActiveRecord::Migration[6.0]
+  def change
+    create_table :videos do |t|
+      t.string :name
+      t.string :url
+      t.integer :likes
+      t.belongs_to :user
+      t.timestamps
+    end
+  end
+end
