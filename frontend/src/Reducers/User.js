1d43193664bfbd06c8c36529049189711b89774d
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-11-05 15:22:01 -0700

    Built out User reducer

--- frontend/src/Reducers/User.js
+++ frontend/src/Reducers/User.js
@@ -1,18 +1,53 @@
+const url = 'http://localhost:3000/';
+
 export const login = (user) => ({
     type: 'LOGIN',
     user
 });
 
+export const pending = () => ({
+    type: 'PENDING'
+})
+
+export const rejected = () => ({
+    type: 'REJECTED'
+})
+
+export const sendLogin = (user) => {
+    return (dispatch) => {
+        dispatch(pending);
+        let formData = {
+            user
+          };
+          let configObj = {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Accept": "application/json"
+            },
+            body: JSON.stringify(formData)
+          };
+      
+          fetch(url + "login", configObj).then(response => response.json()).then(json => {
+              json.message === 'User Found' ? dispatch(login(json.user)) : dispatch(rejected);
+          });
+    }
+}
+
 export const logout = () => ({
     type: 'LOGOUT'
 }) 
 
-export default function user(state=null, action){
+export default function user(state={user: null, pending: false, accepted: false, rejected: false}, action){
     switch(action.type){
         case 'LOGIN':
-            return ({user: action.user});
+            return {user: action.user, accepted: true, pending: false, rejected: false};
         case 'LOGOUT':
-            return null
+            return {...state, user: null}
+        case 'PENDING':
+            return {...state, pending: true, accepted: false, rejected: false};
+        case 'REJECTED':
+            return {...state, rejected: true, accepted: false, pending: false};
         default:
             return state;
     }
