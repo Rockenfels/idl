44d247c48289f75cdfde4fa9d445d1f1fa830797
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-11-05 09:28:38 -0700

    fixed component imports and added Home component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
-import Footer from './Componenets/Display/Footer';
-import NavBar from './Componenets/Display/NavBar';
+import Footer from './Components/Display/Footer';
+import NavBar from './Components/Display/NavBar';
 
 function App() {
   return (
     <div className="app">
       <NavBar />
-      
-
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/:user">
+          <User />
+        </Route>
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
---
+++ frontend/src/Components/Display/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+        <div>
+            <h1>Welcome to Idl!</h1>
+            <p>
+                    The phrase "The devil makes work for idle hands to do" has been around for centuries, but
+                its meaning can still be applied in a modern context. Finding yourself worrying without cause, 
+                your mind wandering to darker corners, and your body pacing as you try to outwalk the shadows 
+                chasing you are all common feelings to those who struggle with anxiety, depression, attention
+                deficit disorders, and other mental health challenges. With any or all of those feelings taking
+                up space in your mind, it can be difficult to put them aside and even easier to find the first 
+                distraction (positive or otherwise) that will help take your mind off your troubles.
+
+                    Idl was created to give users positive outlets for their stress by providing alternatives
+                to wearing a circular path in both your mind and floor. Here we encourage people to take that 
+                energy and channel it into creative/constructive outlets through educational and enabling
+                videos. You'll find videos on here that cover topics ranging from mountain biking and homesteading
+                tutorials, to yoga practices, craft instructionals, and everything in between. Learn something
+                new about what you're already passionate about or broaden your horizons- whatever peaks your 
+                interests and keeps you from idle worry and anxiety.
+
+                Get up. Get active. Get better.
+            </p>
+            <Link exact to="/videos" >Get Started</Link>
+        </div>
+    )
+}
