f9abdd1f84330c52e9b34bb76ffd44ff6de4d504
Ray O <52436369+Rockenfels@users.noreply.github.com>
2020-12-08 09:43:02 -0700

    Added in devise db columns and initializer config

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, skip: :sessions
   resources :videos, only: [:index, :create, :destroy]
   resources :users, except: [:new, :edit, :show]
 
   post '/users/signup', to: 'users#signup';
   post '/users/login', to: 'users#login';
   patch '/users/edit', to: 'users#edit';
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,15 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_210404) do
+ActiveRecord::Schema.define(version: 2020_12_04_230444) do
 
   create_table "users", force: :cascade do |t|
     t.string "uid"
     t.string "username"
-    t.string "email"
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "unconfirmed_email"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "videos", force: :cascade do |t|
